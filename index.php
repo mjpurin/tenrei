4375de1994565397279ebfe54e66adb9f3437883
mjpurin <mjpurin@gmail.com>
2022-01-12 20:54:37 +0900

    navまで実装

--- index.php
+++ index.php
@@ -4,13 +4,82 @@
         <meta charset="utf-8">
         <title>タイトル</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/reset.css">
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
-        <!-- ここにコンテンツ -->
-        <p>コンテンツ</p>
-      <script src="js/vendor/jquery-1.12.4.min.js"></script>
+        <div class="container">
+            <header>
+                <img id="headerLogo" src="images/ansin-logo.png" alt="株式会社 安心典礼">
+                <p>年中無休・24時間対応　TEL:044-975-3853 (代表)　事前相談、無料でお受けいたします</p>
+            </header>
+            <nav>
+            <ul>
+      <li><a href="#">Home</a></li>
+      <li class="has-child"><a href="#">価格別葬儀祭壇</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">少額短期葬儀保険</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">ホール・葬儀場</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">寺院・霊園・石材店</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">葬儀の知識</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">暮らし</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">お問い合わせ</a> </li>
+    </ul>
+            </nav>
+            <main></main>
+            <footer></footer>
+        </div><!-- end container -->
+    
+
+      <!--js  -->
+      <scrit src="js/vendor/jquery-1.12.4.min.js"></script>
       <script src="js/vendor/jquery-ui-1.12.1.min.js"></script>
       <script src="js/vendor/jquery.easing.1.3.js"></script>
       <script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 $(function(){
-
+    $(".has-child>a").off('click');//has-childクラスがついたaタグのonイベントをoff(無効)にし
+    $(".has-child>a").removeClass('active');//activeクラスを削除
+    $('.has-child').children('ul').css("display","");//スライドトグルで動作したdisplayも無効化にする
 });
