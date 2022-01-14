3b7e98dd5c66144914c20c59da7f3a45d85518fa
mjpurin <mjpurin@gmail.com>
2022-01-15 00:58:11 +0900

    index.phpに分割,sub.phpスライダーまで

---
+++ foot.php
@@ -0,0 +1,34 @@
+<footer>
+        <div id="footerMenu">
+            <ul>
+                <li><a href="">ホーム</a></li>
+                <li><a href="">直葬/1日葬/家族葬</a></li>
+                <li><a href="">川崎市市民葬とは？</a></li>
+                <li><a href="">価格別祭壇セット</a></li>
+                <li><a href="">今月のお話</a></li>
+            </ul>
+            <ul>
+                <li><a href="">フューネラルホール（自社）</a></li>
+                <li><a href="">企業提携先一覧</a></li>
+                <li><a href="">一般葬祭場・式場一覧</a></li>
+                <li><a href="">寺院・霊園・石材店ご案内</a></li>
+            </ul>
+            <ul>
+                <li><a href="">役立つ葬儀ガイド</a></li>
+                <li><a href="">葬儀の豆知識</a></li>
+                <li><a href="">会社概要<span>（経営理念・プライバシーポリシー）</span></a></li>
+                <li><a href="">お問い合わせ</a></li>
+            </ul>
+            <ul>
+                <li><a href="">短期少額葬儀保険</a></li>
+                <li><a href="">安心の会</a></li>
+                <li><a href="">忍ぶ会承ります</a></li>
+                <li><a href="">行政関係（弁護士・行政書士）</a></li>
+                <li><a href="">老人ホーム・介護施設</a></li>
+            </ul>
+        </div>
+        <div id="copyRight">Copyright   株式会社 安心典礼  -All Rights Reserved-</div>
+    </footer>
+</div><!-- end container -->    
+    </body>
+</html>
---
+++ head.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>タイトル</title>
+        <link rel="stylesheet" href="css/reset.css">
+        <link rel="stylesheet" href="css/slick.css">
+        <link rel="stylesheet" href="css/slick-theme.css">
+        <link rel="stylesheet" href="css/main.css">
+         <!--js  -->
+      <script  src="js/vendor/jquery-1.12.4.min.js"></script>
+      <script  src="js/vendor/jquery-ui-1.12.1.min.js"></script>
+      <script  src="js/vendor/jquery.easing.1.3.js"></script>
+      <script  src="js/main.js"></script>
+    
+    </head>
+    <body onload="init()">
+        <div class="container">
+            <header>
+                <img id="headerLogo" src="images/ansin-logo.png" alt="株式会社 安心典礼">
+                <p>年中無休・24時間対応　TEL:044-975-3853 (代表)　事前相談、無料でお受けいたします</p>
+            </header>
+            <nav>
+            <ul>
+      <li><a href="index.php">Home</a></li>
+      <li class="has-child"><a href="sub.php">価格別葬儀祭壇</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">少額短期葬儀保険</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">ホール・葬儀場</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">寺院・霊園・石材店</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">葬儀の知識</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">暮らし</a>
+        <ul>
+        <li><a href="#">直葬/1日葬/家族葬</a></li>
+        <li><a href="#">やや費用を抑えたい</a></li>
+        <li><a href="#">一般的な葬儀価格帯</a></li>
+        <li><a href="#">立派にしてあげたい</a></li>
+        <li><a href="#">川崎市市民葬儀</a></li>
+        </ul>
+      </li>
+      <li class="has-child"><a href="#">お問い合わせ</a> </li>
+    </ul>
+    </nav>
