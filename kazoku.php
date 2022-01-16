8f4887fe9edaab2a3af1cab56ddfa569daf3f792
mjpurin <mjpurin@gmail.com>
2022-01-16 22:36:25 +0900

    kazoku,section１列OK

---
+++ kazoku.php
@@ -0,0 +1,49 @@
+<?php include('head.php');?>
+<div id="smallHeader">
+  <img src="<?=$root?>images/headerKazoku.png">
+</div>
+<main id="kazoku">
+<section class="part green odd">
+  <div class="imgContainer">
+    <div class="imgBox">
+      <img src="<?=$root?>images/kazoku/kazoku1.png">
+      <h3>祭壇例（フラワー祭壇6尺）</h3>
+      <p>様々なフラワー祭壇がございます。詳細はお問い合わせください。</p>
+    </div>
+    <button>家族葬プランのお問い合わせはこちら</button>
+
+  </div>
+  <div class="info">
+    <h1>家族葬プラン<span>429,000円（税込）</span>
+    <h2>通夜・告別式</h2>
+    <p>ご家族やご親族で故人さまを偲ぶ、葬儀プランです。身近な方たちだけでお別れの時間をゆっくり取っていただけるのが特徴です。安心典礼ならではの心のこもったサポートを提供させていただきます。</p>
+    <div class="boxes">
+      <ul>
+        <li>寝　棺（桐六分、布団、仏衣付き）</li>
+        <li>寝台車（基本10km,吸水シート付き）</li>
+        <li>霊柩車（火葬場までの基本10km）</li>
+        <li>ドライアイス10kg　1回分</li>
+      </ul>
+      <ul class="partGrid">
+        <li class="double two">白木<br>位牌</li>
+        <li class="two">遺影</li>
+        <li>枕飾り</li>
+        <li>受付台</li>
+        <li>焼香台</li>
+        <li class="double two">役所<br>手続</li>
+        <li class="double two grey">収骨<br>容器</li>
+        <li class="grey">火葬費</li>
+        <li class="double grey">葬祭場<br>利用費</li>
+      </ul>
+    </div>
+    <p class="caution">
+    ※緑枠の内容が家族葬プランに含まれております。<br>
+    ※火葬費用・・・実費料金となります。<br>
+    ※葬祭場利用費・・・弊社フューネラルホールは1日利用で110,000円（税込）<br>
+    <span>その他の葬祭場利用は実費料金となります。</span>
+    </p>
+
+  </div>
+</section>
+</main>
+<?php include('foot.php');?> 
---
+++ subSmall.php
@@ -0,0 +1,8 @@
+<?php include('head.php');?>
+<div id="smallHeader">
+  <img src="<?=$root?>images/headerKazoku.png">
+</div>
+<main>
+
+</main>
+<?php include('foot.php');?> 
