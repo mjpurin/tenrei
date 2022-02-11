bc7353f179a47f89ec7254432fbc19d05a541f51
mjpurin <mjpurin@gmail.com>
2022-02-12 00:02:06 +0900

    ホールrokkakuまで

---
+++ hall.php
@@ -0,0 +1,63 @@
+<?php include('head.php');?>
+<main id="hall">
+<h1><img src="<?=$root?>images/hall/h1.jpg" alt="自社葬祭場フューネラルホールのご案内"></h1>
+<h2>人生最後のセレモニーを心を込めてサポートいたします</h2>
+<p class="h2P">
+2020年6月にリニューアルオープンした弊社1Fのフューネラルホールは宗教や宗派を問わずにご利用いただけます。ホールの収容可能人数は約30名で、近年需要が多い家族葬・一日葬・直葬等シンプルでアットホームなご葬儀に最適なホールです。心を込めてお見送りのお手伝いをさせていただきます。 </p>
+<div id="hallWrapper">
+  <h3>ホールの詳細</h3>
+  <img src="<?=$root?>images/hall/hall.jpg">
+  <div id="hallBanner">
+    <div id="bannerImgs">
+      <img src="<?=$root?>images/hall/banner1.png">
+      <img src="<?=$root?>images/hall/banner2.png">
+      <img src="<?=$root?>images/hall/banner3.png">
+    </div>
+    <p>フラワー/白木祭壇12尺まで設置可能・搬出入しやすい開口の大きさ</p>
+    <ul>
+      <li>宗教宗派<br>不問</li>
+      <li>収容人数<br>約 30 名</li>
+      <li>家族葬に<br>最適空間</li>
+      <li>12尺祭壇<br>設置可能</li>
+      <li>偲ぶ映像<br>モニター</li>
+      <li>コロナ<br>対策実施</li>
+    </ul>
+  </div>
+  <div id="rokkaku">
+    <div id="ozashiki">
+      <h4>お座敷のご利用</h4>
+      <p>2Fの座敷はお清め<br>会場として利用<br>
+  　　可能です</p>
+    </div>
+    <div id="onkyo">
+      <h4>音響設備</h4>
+      <p>音響設備も一新し、<br>
+      厳かな式を演出します
+      </p>
+    </div>
+    <div id="seika">
+      <h4>生花・供花</h4>
+      <p>生花・供花の手配<br>
+      も承ります
+      </p>
+    </div>
+    <div id="butsugu">
+      <h4>仏具・備品</h4>
+      <p>ご葬儀に必要な仏具や<br>
+      備品もご用意しています
+      </p>
+    </div>
+    <div id="gyosha">
+      <h5>関係先指定業者</h5>
+      <ul>
+        <li>ビューティー花壇<br>（生花祭壇・供花）</li>
+        <li>春夏秋冬<br>（料理全般）</li>
+        <li>東京あじよし商事<br>（受付用具・案内板・収骨容器）</li>
+      </ul>
+    </div>
+
+  </div><!-- rokkakuEnd -->
+</div><!-- hallWrapperEnd -->
+</main>
+
+<?php include('foot.php');?> 
