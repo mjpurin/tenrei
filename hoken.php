1a3d2a973723ca69f9072839ac68add893dc610c
mjpurin <mjpurin@gmail.com>
2022-03-09 10:40:38 +0900

    保険ページ追加

---
+++ hoken.php
@@ -0,0 +1,201 @@
+<?php include('head.php');?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500&display=swap" rel="stylesheet">
+<div id="hokenWrapper">
+<div id="smallHeader">
+  <img src="<?=$root?>images/hoken/headerBG.png">
+</div>
+<div id="hokenContainer">
+<main id="hoken">
+  <section id="top">
+    <h2>ごあんしん共済<span>健康に自信のある方</span></h2>
+    <h2>おそうしき共済<span>持病がある方</span></h2>
+    <div id="banner">正規代理店　株式会社 安心典礼</div>
+    <div id="illust">
+      <p>
+        <span>健康な人は</span>
+        <span>もちろん</span>
+        <span>健康に不安があっても</span>
+        <span>少額短期保険</span>
+        <span>なら</span>
+        <span>入りやすい!!</span>
+      </p>
+      <p>
+        <span>私も保険に</span>
+        <span class="offset">入りたいけど</span>
+        <span class="offset">もう年だし</span>
+        <span>持病も・・</span>
+      </p>
+    </div>
+    <div id="hokenInfo">
+      <p>Q:少額短期保険とは？<br>A:保険料がお手頃な上限300万円の保険です</p>
+      <a href="">詳しい内容を富士少額短期保険株式会社の公式サイトで見る</a>
+    </div>
+  </section>
+  <section id="second">
+    <h2>弊社では、万一に備えて安心な保険をお取り扱いしています</h2>
+    <h3>お手頃な保険料</h3>
+    <h4>持病があっても、過去に入院・手術歴があっても<p>医師の<br>審査不要</p></h4>
+    <h5><span>89</span>歳まで申込み可能!!</h5>
+    <p>※ごあんしん共済は健康に自信のある方へお薦めの保険です。<br>
+    ※おそうしき共済は持病、過去の入院・手術歴がある方でも入りやすい保険です。<br>
+    ※どちらの共済も、保険金の使い道は自由です。</p>
+    <ul>
+      <li>選べる<br>5つの<br>プラン</li>
+      <li>病気<br>事故<br>感染症</li>
+      <li>負担が<br>軽い<br>保険料</li>
+      <li>診断書<br>提出<br>不要</li>
+    </ul>
+  </section>
+  <section id="third">
+     <h2>ごあんしん共済・おそうしき共済は<br><span>5つのプランからお選びいただけます</span></h2>
+     <h3>葬儀費用の目安からみた各プラン</h3>
+     <ul>
+       <li><div>300万円</div><p>社葬<br>など</p></li>
+       <li><div>200万円</div><p>立派な<br>ご葬儀</p></li>
+       <li><div>150万円</div><p>一般的<br>ご葬儀</p></li>
+       <li><div>100万円</div><p>家族葬<br>一日葬</p></li>
+       <li><div>50万円</div><p>直葬<br><span>火葬のみ</span></p></li>
+     </ul>
+  </section>
+  <section id="forth" class="hasTable">
+    <h2><span>健康に自信がある方の</span><span>ごあんしん共済</span><span>(死亡保険)</span></h2>
+    <p>もしもの時に備える、お葬式費用・その他のための保険です。</p>
+    <p class="small">・50～300万円まで選べる死亡保険。<br>
+    ・89歳まで加入でき、100歳まで更新可能！<br>
+    ・<span>保険金の使い道は自由</span>（お葬式費用、遺品の整理費用、残されたご家族の生活費用等に）</p>
+    <h3>月々のお支払い例　<span>ごあんしん共済（無配当死亡保険）の場合</span></h3>
+    <table>
+      <tr>
+        <th></th><th colspan="2">男　性</th><th colspan="2">女　性</th>
+      </tr>
+      <tr>
+        <td>年　齢</td>
+        <td>100 万円プラン</td>
+        <td>300 万円プラン</td>
+        <td>100 万円プラン</td>
+        <td>300 万円プラン</td>
+      </tr>
+      <tr>
+        <td>50 〜 54歳</td>
+        <td>740 円</td>
+        <td>2,220 円</td>
+        <td>470 円</td>
+        <td>1,420 円</td>
+      </tr>
+      <tr>
+        <td>55 〜 59歳</td>
+        <td>1,050 円</td>
+        <td>3,140 円</td>
+        <td>570 円</td>
+        <td>1,700 円</td>
+      </tr>
+      <tr>
+        <td>60 〜 64歳</td>
+        <td>1,490 円</td>
+        <td>4,460 円</td>
+        <td>740 円</td>
+        <td>2,210 円</td>
+      </tr>
+      <tr>
+        <td>65 〜 69歳</td>
+        <td>2,380 円</td>
+        <td>7,140 円</td>
+        <td>1,090 円</td>
+        <td>3,260 円</td>
+      </tr>
+      <tr>
+        <td>70 〜 74歳</td>
+        <td>3,810 円</td>
+        <td>11,420 円</td>
+        <td>1,700 円</td>
+        <td>5,100 円</td>
+      </tr>
+    </table>
+    <a href="">ごあんしん共済、おそうしき共済のお問い合わせ・お申込みはこちら（安心典礼）</a>
+    <a href="">詳しい内容を富士少額短期保険株式会社の公式サイトでご覧になる方はこちら</a>
+  </section>
+  <section id="fifth" class="hasTable">
+    <h2><span>持病がある方でも入りやすい</span><span>おそうしき共済</span><span>（死亡/緩和型）</span></h2>
+    <p>持病のある方向けのお葬式費用のための保険です。</p>
+    <p class="small">・50～300万円まで選べる死亡保険。<br>
+    ・89歳まで加入でき、99歳まで更新可能！<br>
+    ・<span>現在、持病の治療中（服薬のみを含みます）の方、</span>ご自分の葬儀費用を準備しておきたい方へお薦めです。</br>
+    ・<span>保険金の使い道は自由</span>（お葬式費用、遺品の整理費用、残されたご家族の生活費用等に）</p>
+    <h3>月々のお支払い例　<span>ごあんしん共済（無配当死亡保険）の場合</span></h3>
+    <table>
+      <tr>
+        <th></th><th colspan="2">男　性</th><th colspan="2">女　性</th>
+      </tr>
+      <tr>
+        <td>年　齢</td>
+        <td>100 万円プラン</td>
+        <td>300 万円プラン</td>
+        <td>100 万円プラン</td>
+        <td>300 万円プラン</td>
+      </tr>
+      <tr>
+        <td>50 〜 54歳</td>
+        <td>1,530 円</td>
+        <td>4,580 円</td>
+        <td>1,260 円</td>
+        <td>3,780 円</td>
+      </tr>
+      <tr>
+        <td>55 〜 59歳</td>
+        <td>1,970 円</td>
+        <td>5,920 円</td>
+        <td>1,430 円</td>
+        <td>4,290 円</td>
+      </tr>
+      <tr>
+        <td>60 〜 64歳</td>
+        <td>2,610 円</td>
+        <td>7,840 円</td>
+        <td>1,740 円</td>
+        <td>5,220 円</td>
+      </tr>
+      <tr>
+        <td>65 〜 69歳</td>
+        <td>3,920 円</td>
+        <td>11,750 円</td>
+        <td>2,370 円</td>
+        <td>7,120 円</td>
+      </tr>
+      <tr>
+        <td>70 〜 74歳</td>
+        <td>5,990 円</td>
+        <td>17,980 円</td>
+        <td>3,490 円</td>
+        <td>10,470 円</td>
+      </tr>
+    </table>
+    <h3>4つの告知で簡単にお申込みいただけます。（診断書の提出不要）</h3>
+    <div>
+      <div>
+        <p>下記の4つの項目が「いいえ」に該当するかご確認ください。</p>
+        <p>（5年以内にがん診断がある方、『がんになっても入れるほけん』がございます。相談ください）</p>
+      </div>
+      <ul>
+        <li>現在入院中、もしくは最近6ヶ月以内に医師による診察を受けた結果、入院・手術・または検査をすすめられていますか？<br>
+        <span>※検査結果が判明し、入院・手術・再検査の必要がなかった場合は「いいえ」とお答えください。</span></li>
+        <li>過去1年以内に、病気やケガで継続して7日以上入院をした、または手術を受けたことがありますか？</li>
+        <li>現在、公的介護保険制度による要介護認定3～5の認定を受けていますか？</li>
+        <li>過去5年以内に下記の病気で医師の診察・検査・治療・投薬を受けたことがありますか？</li>
+      </ul>
+      <p>
+      がん（※）/狭心症/心筋梗塞/心筋症/心臓弁膜症/慢性肝炎/肝硬変/慢性膵炎/腎不全<br>
+      慢性閉塞性肺疾患（肺気腫、慢性気管支炎を含む）/間質性肺炎/肺線維症<br>
+      糖尿病の合併症（網膜症、腎症、下肢の潰瘍や壊疽）/指定難病（難病医療費助成の対象）<br>
+      ※がんとは、上皮内新生物（高度異形成、上皮内がんを含む）、白血病、悪性リンパ腫、肉腫、<br>
+      GIST等を含む悪性腫瘍を言います。
+      </p>
+    </div>
+    <a href="">ごあんしん共済、おそうしき共済のお問い合わせ・お申込みはこちら（安心典礼）</a>
+    <a href="">詳しい内容を富士少額短期保険株式会社の公式サイトでご覧になる方はこちら</a>
+  </section>
+</main>
+</div>
+</div>
+<?php include('foot.php');?> 
